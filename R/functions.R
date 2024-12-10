a9d7939d35aa120fbdb725479aed6d8a3933be9f
Rebecca Lynn McIntyre <rebeccamcintyre28@gmail.com>
2024-12-10 16:16:47 +0100

    made function for summary statistics

---
+++ R/functions.R
@@ -0,0 +1,21 @@
+#'descriptive_statistics
+#'
+#' @param data is the data file you are starting from
+#'
+#' @return a data frame or tibble of descriptive statistics
+
+
+descriptive_statistics <- function(data) {
+    data |>
+        dplyr::group_by(metabolite) |>
+        dplyr::summarise(dplyr::across(
+            value,
+            list(
+                mean = mean,
+                st_dev = sd
+            )
+        )) |>
+        dplyr::mutate(dplyr::across(
+            where(is.numeric),
+            ~ round(.x, digits = 1)
+        ))}
