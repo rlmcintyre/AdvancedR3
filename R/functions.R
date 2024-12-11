6f3828545b3d41a6946a4668d5e27da50eab6bd0
Rebecca Lynn McIntyre <rebeccamcintyre28@gmail.com>
2024-12-11 12:10:31 +0100

    functions for stats

--- R/functions.R
+++ R/functions.R
@@ -6,17 +6,9 @@
 descriptive_stats <- function(data) {
     data |>
         dplyr::group_by(metabolite) |>
-        dplyr::summarise(dplyr::across(
-            value,
-            list(
-                mean = mean,
-                st_dev = sd
-            )
-        )) |>
-        dplyr::mutate(dplyr::across(
-            tidyselect::where(is.numeric),
-            ~ round(.x, digits = 1)
-        ))}
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+}
 
 
 ## Figure outputs
